cde11bfc1bd00adfe2355d6d2d4e7bca6c4d8382
tzhen1 <tzhen80@gmail.com>
2021-04-06 20:01:54 +0100

    feat: add setup

--- FM-Radio.ino
+++ FM-Radio.ino
@@ -8,12 +8,19 @@
  ******************************************************************************
  */
 #include "ar1010lib.h"
+#include <Wire.h>
 
 AR1010 radio = AR1010(); // instance of ar1010
 void setup() 
 {
-  // put your setup code here, to run once:
-
+  // Enable I2C on the Arduino, including pull-up resistors.
+  Wire.begin();
+  // Initialize the serial port at a speed of 9600 baud
+  Serial.begin(9600); //begins
+  radio.initialise();//Initialise the AR1010 instance of radio to get the class items
+  delay(1000); //1sec delay
+  radio.setFrequency(910); // set it to 91.0MHz
+  Serial.println(radio.frequency());   
 }
 
 void volumeUP()
