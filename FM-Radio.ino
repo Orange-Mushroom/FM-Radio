5588d79c8b5f3ce09c23679739569361ece937c4
tzhen1 <tzhen80@gmail.com>
2021-04-09 12:42:04 +0100

    add classical music

--- FM-Radio.ino
+++ FM-Radio.ino
@@ -22,13 +22,26 @@ AR1010 radio = AR1010(); // instance of ar1010
   radio.initialise();//Initialise the AR1010 instance of radio to get the class items
   delay(1000); //1sec delay
   radio.setFrequency(910); // set it to 91.0MHz
-  radio.setVolume(1);
+  radio.setVolume(2);
   delay(1000);
   pinMode(buttonPin, INPUT); //sets digital pin 3 as input
   Serial.println(radio.frequency());   
   
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(buttonPin)==LOW) //if digital pin on, current run through
+  {
+    radio.seek();
+    delay(500); 
+    Serial.println(radio.frequency());  
+  }
+   Serial.println(digitalRead(buttonPin));  
+   delay(1000);
+
 } */
-double frequencyFM = 910;
+
+double frequencyFM = 1009;//910 100.9
 void setup() {
  Wire.begin();
  Serial.begin(9600);
@@ -37,6 +50,13 @@ void setup() {
  radio.setVolume(5);
  radio.setFrequency(frequencyFM);
 }
+
+void loop() {
+ Serial.print("Playing: ");
+ Serial.println(radio.frequency());
+ delay(60*1000);
+} 
+
 
 void volumeUP()
 {
@@ -54,21 +74,3 @@ void frequencyDown()
 {
   
 }
-void loop() {
- Serial.print("Playing: ");
- Serial.println(radio.frequency());
- delay(60*1000);
-}
-
-/*void loop() {
-  // put your main code here, to run repeatedly:
-  if(digitalRead(buttonPin)==HIGH) //if digital pin on, current run through
-  {
-    radio.seek();
-    delay(500); 
-    Serial.println(radio.frequency());  
-  }
-   Serial.println(digitalRead(buttonPin));  
-   delay(1000);
-
-}*/
